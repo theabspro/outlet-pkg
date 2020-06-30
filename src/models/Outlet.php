d4e6c0b37f68a22316005291c7982c9f62ae5273
arun-uitoux <arunkumar@uitoux.in>
2020-06-30 20:54:44 +0530

    model import changes

--- src/models/Outlet.php
+++ src/models/Outlet.php
@@ -9,6 +9,8 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Outlet extends BaseModel {
 	use SeederTrait;
 	use SoftDeletes;
+	public static $AUTO_GENERATE_CODE = false;
+
 	protected $table = 'outlets';
 	protected $fillable = [
 		'id',
@@ -22,4 +24,131 @@ class Outlet extends BaseModel {
 		'state_id',
 	];
 
+	protected static $excelColumnRules = [
+		'Outlet Group Name' => [
+			'table_column_name' => 'outlet_group_id',
+			'rules' => [
+				'fk' => [
+					'class' => 'App\OutletGroup',
+					'foreign_table_column' => 'name',
+				],
+			],
+		],
+		'Outlet Code' => [
+			'table_column_name' => 'code',
+			'rules' => [
+				'required' => [
+				],
+			],
+		],
+		'Outlet Name' => [
+			'table_column_name' => 'name',
+			'rules' => [
+				'required' => [
+				],
+			],
+		],
+		'State Code' => [
+			'table_column_name' => 'state_id',
+			'rules' => [
+				'required' => [
+				],
+				'fk' => [
+					'class' => 'App\State',
+					'foreign_table_column' => 'code',
+				],
+			],
+		],
+		'Region Code' => [
+			'table_column_name' => 'region_id',
+			'rules' => [
+				'nullable' => [
+				],
+				'fk' => [
+					'class' => 'App\Region',
+					'foreign_table_column' => 'code',
+				],
+			],
+		],
+		'Address' => [
+			'table_column_name' => 'address',
+			'rules' => [
+				'nullable' => [
+				],
+			],
+		],
+		'Pincode' => [
+			'table_column_name' => 'pincode',
+			'rules' => [
+				'nullable' => [
+				],
+			],
+		],
+		'Central Cashier' => [
+			'table_column_name' => 'central_cashier_id',
+			'rules' => [
+				'nullable' => [
+				],
+				'fk' => [
+					'class' => 'App\User',
+					'foreign_table_column' => 'name',
+				],
+			],
+		],
+	];
+
+	public static function saveFromObject($record_data) {
+		$record = [
+			'Company Code' => $record_data->company_code,
+			'Outlet Group Name' => $record_data->outlet_group_name,
+			'Outlet Code' => $record_data->outlet_code,
+			'Outlet Name' => $record_data->outlet_name,
+			'State Code' => $record_data->state_code,
+			'Region Code' => $record_data->region_code,
+			'Address' => $record_data->address,
+			'Pincode' => $record_data->pincode,
+			'Central Cashier' => $record_data->central_cashier,
+		];
+		return static::saveFromExcelArray($record);
+	}
+
+	public static function saveFromExcelArray($record_data) {
+		$errors = [];
+		$company = Company::where('code', $record_data['Company Code'])->first();
+		if (!$company) {
+			return [
+				'success' => false,
+				'errors' => ['Invalid Company : ' . $record_data['Company Code']],
+			];
+		}
+
+		if (!isset($record_data['created_by'])) {
+			$admin = $company->admin();
+
+			if (!$admin) {
+				return [
+					'success' => false,
+					'errors' => ['Default Admin user not found'],
+				];
+			}
+			$created_by = $admin->id;
+		} else {
+			$created_by = $record_data['created_by'];
+		}
+
+		$record = self::firstOrNew([
+			'company_id' => $company->id,
+			'code' => $record_data['Outlet Code'],
+		]);
+
+		$result = Self::validateAndFillExcelColumns($record_data, Static::$excelColumnRules, $record);
+		if (!$result['success']) {
+			return $result;
+		}
+		$record->created_by_id = $created_by;
+		$record->save();
+		return [
+			'success' => true,
+		];
+	}
 }
--- src/models/Shift.php
+++ src/models/Shift.php
@@ -3,12 +3,11 @@
 namespace Abs\OutletPkg;
 
 use Abs\HelperPkg\Traits\SeederTrait;
+use App\BaseModel;
 use App\Company;
-use App\Config;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Shift extends Model {
+class Shift extends BaseModel {
 	use SeederTrait;
 	use SoftDeletes;
 	protected $table = 'shifts';
@@ -33,41 +32,116 @@ class Shift extends Model {
 		return $this->belongsToMany('App\Outlet', 'outlet_shift', 'shift_id', 'outlet_id');
 	}
 
-	public static function createFromObject($record_data) {
+	protected static $excelColumnRules = [
+		'Name' => [
+			'table_column_name' => 'name',
+			'rules' => [
+				'required' => [
+				],
+			],
+		],
+	];
 
-		$errors = [];
-		$company = Company::where('code', $record_data->company)->first();
-		if (!$company) {
-			dump('Invalid Company : ' . $record_data->company);
-			return;
-		}
-
-		$admin = $company->admin();
-		if (!$admin) {
-			dump('Default Admin user not found');
-			return;
-		}
-
-		$type = Config::where('name', $record_data->type)->where('config_type_id', 89)->first();
-		if (!$type) {
-			$errors[] = 'Invalid Tax Type : ' . $record_data->type;
-		}
-
-		if (count($errors) > 0) {
-			dump($errors);
-			return;
-		}
-
-		$record = self::firstOrNew([
-			'company_id' => $company->id,
-			'name' => $record_data->tax_name,
-		]);
-		$record->type_id = $type->id;
-		$record->created_by_id = $admin->id;
-		$record->save();
-		return $record;
+	public static function saveFromObject($record_data) {
+		$record = [
+			'Company Code' => $record_data->company_code,
+			'Name' => $record_data->name,
+		];
+		return static::saveFromExcelArray($record);
 	}
 
+	public static function saveFromExcelArray($record_data) {
+		try {
+			$errors = [];
+			$company = Company::where('code', $record_data['Company Code'])->first();
+			if (!$company) {
+				return [
+					'success' => false,
+					'errors' => ['Invalid Company : ' . $record_data['Company Code']],
+				];
+			}
+
+			if (!isset($record_data['created_by_id'])) {
+				$admin = $company->admin();
+
+				if (!$admin) {
+					return [
+						'success' => false,
+						'errors' => ['Default Admin user not found'],
+					];
+				}
+				$created_by_id = $admin->id;
+			} else {
+				$created_by_id = $record_data['created_by_id'];
+			}
+
+			if (count($errors) > 0) {
+				return [
+					'success' => false,
+					'errors' => $errors,
+				];
+			}
+
+			$record = Self::firstOrNew([
+				'company_id' => $company->id,
+				'name' => $record_data['Name'],
+			]);
+
+			$result = Self::validateAndFillExcelColumns($record_data, Static::$excelColumnRules, $record);
+			if (!$result['success']) {
+				return $result;
+			}
+
+			$record->company_id = $company->id;
+			$record->created_by_id = $created_by_id;
+			$record->save();
+			return [
+				'success' => true,
+			];
+		} catch (\Exception $e) {
+			return [
+				'success' => false,
+				'errors' => [
+					$e->getMessage(),
+				],
+			];
+		}
+	}
+	/*public static function createFromObject($record_data) {
+
+			$errors = [];
+			$company = Company::where('code', $record_data->company)->first();
+			if (!$company) {
+				dump('Invalid Company : ' . $record_data->company);
+				return;
+			}
+
+			$admin = $company->admin();
+			if (!$admin) {
+				dump('Default Admin user not found');
+				return;
+			}
+
+			$type = Config::where('name', $record_data->type)->where('config_type_id', 89)->first();
+			if (!$type) {
+				$errors[] = 'Invalid Tax Type : ' . $record_data->type;
+			}
+
+			if (count($errors) > 0) {
+				dump($errors);
+				return;
+			}
+
+			$record = self::firstOrNew([
+				'company_id' => $company->id,
+				'name' => $record_data->tax_name,
+			]);
+			$record->type_id = $type->id;
+			$record->created_by_id = $admin->id;
+			$record->save();
+			return $record;
+		}
+	*/
 	public static function getList($params = [], $add_default = true, $default_text = 'Select Shift') {
 		$list = Collect(Self::select([
 			'id',
